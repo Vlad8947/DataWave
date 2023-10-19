ce773e8926d82d7b811d06689b948014c3612dca
Goncharov Vladislav <31621413+Vlad8947@users.noreply.github.com>
2023-10-19 03:08:48 +0300

    init demo

---
+++ build.gradle.kts
@@ -0,0 +1,42 @@
+plugins {
+    id("java")
+    id("org.springframework.boot") version "3.1.4"
+    id("io.spring.dependency-management") version "1.1.3"
+}
+
+group = "net.mirror"
+version = "1.0-SNAPSHOT"
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_17
+}
+
+configurations {
+    compileOnly {
+        extendsFrom(configurations.annotationProcessor.get())
+    }
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    compileOnly("org.projectlombok:lombok")
+    annotationProcessor("org.projectlombok:lombok")
+
+    implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.2.0")
+
+    developmentOnly("org.springframework.boot:spring-boot-devtools")
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
+//    runtimeOnly("io.micrometer:micrometer-registry-influx")
+
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation(platform("org.junit:junit-bom:5.9.1"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
---
+++ src/main/java/net/mirror/DataWaveApp.java
@@ -0,0 +1,12 @@
+package net.mirror;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+//@EnableSwagger2
+public class DataWaveApp {
+    public static void main(String[] args) {
+        SpringApplication.run(DataWaveApp.class, args);
+    }
+}
